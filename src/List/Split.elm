a40e74f20d5006d09569f6fde8a91bb1e23ff228
Rehno Lindeque <errantkid@gmail.com>
2015-05-29 11:36:47 +0200

    chunksOfLeft and chunksOfRight

---
+++ src/List/Split.elm
@@ -0,0 +1,27 @@
+module List.Split where
+{-| Split lists into chunks
+
+# Splitters
+@docs chunksOfLeft, chunksOfRight
+
+-}
+
+import List exposing (..)
+
+{-| Split list into smaller lists of length `k`, starting from the left.
+
+    chunksOfLeft 3 [1,2,3,4,5,6,7,8] = [[1,2,3],[4,5,6],[7,8]]
+-}
+chunksOfLeft : Int -> List a -> List (List a)
+chunksOfLeft k xs =
+  let len = length xs
+  in  if len > k
+      then take k xs :: chunksOfLeft k (drop k xs)
+      else [xs]
+
+{-| Split list into smaller lists of length `k`, starting from the right.
+
+    chunksOfRight 3 [1,2,3,4,5,6,7,8] = [[1,2], [3,4,5], [6,7,8]]
+-}
+chunksOfRight : Int -> List a -> List (List a)
+chunksOfRight k = map reverse << chunksOfLeft k << reverse
