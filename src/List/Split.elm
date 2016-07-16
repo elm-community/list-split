68b322ee4ed31ec86200966cc50030e8d5cedbfe
Isaac Shapira <fresheyeball@gmail.com>
2016-07-15 20:26:11 -0600

    handle degenerate cases

--- src/List/Split.elm
+++ src/List/Split.elm
@@ -12,27 +12,31 @@ import List exposing (..)
 
 {-| Split list into smaller lists of length `k`, starting from the left.
 
-    chunksOfLeft 3 [1..9] == [[1,2,3],[4,5,6],[7,8,9]]
-    chunksOfLeft 3 [1,2,3,4,5,6,7,8] == [[1,2,3],[4,5,6],[7,8]]
-    chunksOfLeft 3 [] == [[]]
+    chunksOfLeft  3 [1..9] == [[1,2,3],[4,5,6],[7,8,9]]
+    chunksOfLeft  3 [1,2,3,4,5,6,7,8] == [[1,2,3],[4,5,6],[7,8]]
+    chunksOfLeft  3 [] == [[]]
+    chunksOfLeft  0 xs == [[]]
+    chunksOfLeft -1 xs == []
 -}
 chunksOfLeft : Int -> List a -> List (List a)
 chunksOfLeft k xs =
-    let
-        len =
-            length xs
-    in
-        if len > k then
-            take k xs :: chunksOfLeft k (drop k xs)
-        else
-            [ xs ]
+    if k == 0 then
+        [ [] ]
+    else if k < 0 then
+        []
+    else if length xs > k then
+        take k xs :: chunksOfLeft k (drop k xs)
+    else
+        [ xs ]
 
 
 {-| Split list into smaller lists of length `k`, starting from the right.
 
-    chunksOfRight 3 [1..9] == [[1,2,3],[4,5,6],[7,8,9]]
-    chunksOfRight 3 [1,2,3,4,5,6,7,8] == [[1,2], [3,4,5], [6,7,8]]
-    chunksOfRight 3 [] == [[]]
+    chunksOfRight  3 [1..9] == [[1,2,3],[4,5,6],[7,8,9]]
+    chunksOfRight  3 [1,2,3,4,5,6,7,8] == [[1,2], [3,4,5], [6,7,8]]
+    chunksOfRight  3 [] == [[]]
+    chunksOfRight  0 xs == [[]]
+    chunksOfRight -1 xs == []
 -}
 chunksOfRight : Int -> List a -> List (List a)
 chunksOfRight k =
